06168e88935e46adfc6735a394fb159058e4e7ec
roal <replicaset01@github.com>
2022-12-08 11:06:40 +0900

    Initial Commit

---
+++ src/main/java/com/aws/actiondeploy/ActionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.aws.actiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/actiondeploy/ActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.actiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
